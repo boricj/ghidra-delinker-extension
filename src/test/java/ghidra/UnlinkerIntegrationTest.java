5276a170a9017c467c5a1f9623cfb36257d25f02
Jean-Baptiste Boric <jblbeurope@gmail.com>
2023-08-20 01:00:00 +0200

    test: harness for integration tests

---
+++ src/test/java/ghidra/UnlinkerIntegrationTest.java
@@ -0,0 +1,157 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ghidra;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+
+import db.DBHandle;
+import generic.jar.ResourceFile;
+import ghidra.framework.GModule;
+import ghidra.framework.store.db.PrivateDatabase;
+import ghidra.program.database.ProgramDB;
+import ghidra.program.model.address.Address;
+import ghidra.program.model.address.AddressFactory;
+import ghidra.program.model.address.AddressSet;
+import ghidra.program.model.address.AddressSetView;
+import ghidra.program.model.listing.Program;
+import ghidra.program.model.mem.MemoryBlock;
+import ghidra.test.AbstractProgramBasedTest;
+import ghidra.test.TestProgramManager;
+import ghidra.util.NamingUtilities;
+import ghidra.util.task.TaskMonitor;
+import utility.application.ApplicationLayout;
+
+public abstract class UnlinkerIntegrationTest extends AbstractProgramBasedTest {
+	private static DBHandle dbHandle = null;
+	private static Program program = null;
+
+	public static class IntegrationTestApplicationLayout extends GhidraTestApplicationLayout {
+		public IntegrationTestApplicationLayout(File userSettingsDir)
+				throws FileNotFoundException, IOException {
+			super(userSettingsDir);
+		}
+
+		@Override
+		protected Map<String, GModule> findGhidraModules() throws IOException {
+			Map<String, GModule> modules = new HashMap<>(super.findGhidraModules());
+			modules.put("Unlinker",
+				new GModule(applicationRootDirs, new ResourceFile(System.getProperty("user.dir"))));
+			return Collections.unmodifiableMap(modules);
+		}
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		TestProgramManager.cleanDbTestDir();
+
+		initialize();
+	}
+
+	@Override
+	@After
+	public void tearDown() throws Exception {
+		dbHandle.close();
+		dbHandle = null;
+		program = null;
+
+		TestProgramManager.cleanDbTestDir();
+	}
+
+	@Override
+	protected Program getProgram() throws Exception {
+		if (program != null) {
+			return program;
+		}
+
+		File dbDir = new File(TestProgramManager.getDbTestDir(),
+			NamingUtilities.mangle(getProgramName()) + ".db");
+		File gzf = new File(getProgramName());
+
+		PrivateDatabase pdb = new PrivateDatabase(dbDir, gzf, TaskMonitor.DUMMY);
+		dbHandle = pdb.open(TaskMonitor.DUMMY);
+		program = new ProgramDB(dbHandle, 1, TaskMonitor.DUMMY, this);
+
+		return program;
+	}
+
+	@Override
+	protected ApplicationLayout createApplicationLayout() throws IOException {
+		return new IntegrationTestApplicationLayout(new File(getTestDirectoryPath()));
+	}
+
+	public static AddressSetView getAddressSetOfMemoryBlocks(Program program,
+			List<String> memoryBlockNames) {
+		AddressFactory addressFactory = program.getAddressFactory();
+		AddressSet set = addressFactory.getAddressSet();
+
+		List<MemoryBlock> memoryBlocks =
+			memoryBlockNames.stream().map(n -> program.getMemory().getBlock(n)).toList();
+		for (MemoryBlock memoryBlock : memoryBlocks) {
+			Address start = memoryBlock.getStart();
+			Address end = memoryBlock.getEnd();
+
+			set.add(addressFactory.getAddressSet(start, end));
+		}
+
+		return set;
+	}
+
+	protected void compareElfSectionWithRawFile(File exportedFile, String inputFormat,
+			String actualFilenamePattern, String sectionName) throws Exception {
+		byte[] actualBytes;
+		byte[] expectedBytes;
+
+		String expectedRawFilename = String.format(actualFilenamePattern, sectionName);
+		File expectedRawFile = new File(expectedRawFilename);
+		try (FileInputStream fis = new FileInputStream(expectedRawFile)) {
+			expectedBytes = fis.readAllBytes();
+		}
+
+		File actualRawFile = createTempFileForTest(".raw");
+
+		List<String> objcopyCmdline = List.of(
+			"objcopy",
+			"-I", inputFormat,
+			"-O", "binary",
+			"-j", sectionName,
+			"--set-section-flags", String.format("%s=alloc,load,content", sectionName),
+			exportedFile.getAbsolutePath(),
+			actualRawFile.getAbsolutePath());
+
+		Process process = new ProcessBuilder(objcopyCmdline).start();
+		assertTrue("objdump invocation completes", process.waitFor(10, TimeUnit.SECONDS));
+		assertEquals("objdump invocation succeeded", 0, process.exitValue());
+
+		try (FileInputStream fis = new FileInputStream(actualRawFile)) {
+			actualBytes = fis.readAllBytes();
+		}
+
+		assertArrayEquals(expectedBytes, actualBytes);
+	}
+}

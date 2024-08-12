9d6aa95d9d72169b7d2ea37fb61898ec760907cf
Jean-Baptiste Boric <jblbeurope@gmail.com>
2024-08-12 12:22:30 +0200

    chore: implement ProgramUtil symbol methods

--- src/main/java/ghidra/app/util/ProgramUtil.java
+++ src/main/java/ghidra/app/util/ProgramUtil.java
@@ -13,10 +13,23 @@
  */
 package ghidra.app.util;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Spliterators;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 import ghidra.framework.model.DomainObject;
 import ghidra.program.model.address.Address;
 import ghidra.program.model.address.AddressSetView;
 import ghidra.program.model.listing.Program;
+import ghidra.program.model.relocobj.Relocation;
+import ghidra.program.model.relocobj.RelocationTable;
+import ghidra.program.model.symbol.Symbol;
 
 public class ProgramUtil {
 	public static Program getProgram(DomainObject domainObj) {
@@ -30,5 +43,45 @@ public class ProgramUtil {
 		Address minAddress = addressSet.getMinAddress();
 		AddressSetView intersectedRange = addressSet.intersectRange(minAddress, address);
 		return intersectedRange.getNumAddresses() - 1;
+	}
+
+	public static Map<String, Symbol> getSectionSymbols(Program program,
+			AddressSetView sectionSet) {
+		return getSymbols(program, s -> sectionSet.contains(s.getAddress()), false);
+	}
+
+	public static Map<String, Symbol> getExternalSymbols(Program program, AddressSetView fileSet) {
+		Map<String, Symbol> externalSymbols =
+			getSymbols(program, s -> !fileSet.contains(s.getAddress()), true);
+
+		RelocationTable relocationTable = RelocationTable.get(program);
+		Stream<Relocation> relocations = StreamSupport.stream(
+			Spliterators.spliteratorUnknownSize(relocationTable.getRelocations(fileSet), 0), false);
+		return relocations.map(r -> r.getSymbolName())
+				.filter(s -> s != null && externalSymbols.containsKey(s))
+				.distinct()
+				.collect(Collectors.toMap(Function.identity(), s -> externalSymbols.get(s)));
+	}
+
+	private static Map<String, Symbol> getSymbols(Program program, Predicate<Symbol> predicate,
+			boolean allowDuplicates) {
+		Stream<Symbol> symbols =
+			StreamSupport.stream(program.getSymbolTable().getAllSymbols(true).spliterator(), false);
+		Collection<List<Symbol>> symbolsPerAddress =
+			symbols.filter(predicate)
+					.collect(Collectors.groupingBy(Symbol::getAddress, Collectors.toList()))
+					.values();
+		Stream<Symbol> intermediate = symbolsPerAddress.stream().map(candidates -> {
+			return candidates.stream().filter(c -> c.isPrimary()).findAny().orElseThrow();
+		});
+
+		if (allowDuplicates) {
+			return intermediate.collect(
+				Collectors.toMap(s -> s.getName(true), Function.identity(), (a, b) -> a));
+		}
+		else {
+			return intermediate
+					.collect(Collectors.toMap(s -> s.getName(true), Function.identity()));
+		}
 	}
 }
